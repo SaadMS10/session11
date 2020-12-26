8438b1547595fa5e4c4d63a33cda4eb7f902f10a
Muhammad Saad <itsmsaadhere@gmail.com>
2020-12-26 16:32:22 +0500

    ShoeStore

---
+++ src/Components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import jordan from './113677.jpg';
+
+function Home() {
+var elem;
+var state=true
+
+	
+  return (
+	<div>
+		<h1>Welcome to Home</h1>
+		<img src={jordan}  id="cloud" />
+		
+	
+	
+	
+
+	</div>
+  );
+}
+
+export default Home;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles((theme) => ({
+ 
+  bar:{
+    paddingLeft: "5%",
+    textDecoration: "none"
+  },
+  a:{
+    textDecoration: "none"
+  }
+
+}));
+
+
+function NavBar() {
+  const classes = useStyles();
+
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar >
+        <Link  className={classes.bar} to="/">    <Typography variant="h6" className={classes.title}>
+            HOME
+          </Typography> </Link>
+        <Link className={classes.bar}  to="/about">    <Typography variant="h6" className={classes.title}>
+            ABOUT
+          </Typography> </Link>
+        <Link className={classes.bar} to="/product">    <Typography variant="h6" className={classes.title}>
+            PRODUCT
+          </Typography> </Link>
+         
+      
+  
+       
+        </Toolbar>
+      </AppBar>
+  
+    </div>
+  );
+
+  
+
+
+}
+
+export default NavBar;
