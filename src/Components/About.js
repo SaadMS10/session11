60e592fb3d6fa1fd164c17b93685a0a96c2f628b
Muhammad Saad <itsmsaadhere@gmail.com>
2020-12-31 17:21:51 +0500

    Updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,8 +4,8 @@ function About() {
 
   return (
 	<div>
-		<h1 class="about h1">ABOUT NIKE </h1>
-		<p class="about p">Our mission is what drives us to do everything possible to expand human potential. We do that by creating groundbreaking sport innovations, by making our products more sustainably, by building a creative and diverse global team and by making a positive impact in communities where we live and work.
+		<h1 class="about-h1">ABOUT NIKE </h1>
+		<p class="about-p">Our mission is what drives us to do everything possible to expand human potential. We do that by creating groundbreaking sport innovations, by making our products more sustainably, by building a creative and diverse global team and by making a positive impact in communities where we live and work.
 Based in Beaverton, Oregon, NIKE, Inc. includes the Nike, Converse, and Jordan brands.</p>
 	</div>
   );
